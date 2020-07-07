0634733299f506351d8052ba1e703f073fbc014d
FuryCydonian <ella.bir@hotmail.com>
2020-07-07 19:07:27 +0300

    Initialize a package

---
+++ .venv/Scripts/brain-games-script.py
@@ -0,0 +1,12 @@
+#!e:\programming\hexlet\python-project-lvl1\.venv\scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'furycydonian-brain-games','console_scripts','brain-games'
+__requires__ = 'furycydonian-brain-games'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('furycydonian-brain-games', 'console_scripts', 'brain-games')()
+    )
